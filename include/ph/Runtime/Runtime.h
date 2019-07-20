7d9bdebca87aa762d32b205df5ea9feb48249ea1
Json Lee <lijiansong@ict.ac.cn>
2019-07-20 13:30:21 +0800

    TODO: first step runtime for CPU C

---
+++ include/ph/Runtime/Runtime.h
@@ -0,0 +1,18 @@
+//===--- Runtime.h - "Umbrella" header for Runtime library ------*- C++ -*-===//
+//
+//                     The Phaeton Compiler Infrastructure
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file defines the interface to the Runtime classes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __RUNTIME_H__
+#define __RUNTIME_H__
+
+// TODO: Runtime support for Host memory management and kernel launch,
+// refer to TVM and TensorComprehensions. We may need to add an adapter
+// for CPU, GPU, MLU, TPU and etc.
+
+#endif // __RUNTIME_H__
